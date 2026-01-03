0a7d1305f7a15508a6d8b522dc37148f2c10204d
Shakir Salmani <salmanim281@gmail.com>
2026-01-03 21:28:49 +0530

    Downgraded Sanity to v3 for Vercel fix

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "sk9drqx3", // Aapka Project ID (Terminal se liya hai maine)
+  projectId: "sk9drqx3",
   dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
