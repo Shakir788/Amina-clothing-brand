11beebbbe9222ca5b9df03f7eea0fe02f58dd5d0
Shakir Salmani <salmanim281@gmail.com>
2026-03-08 13:04:32 +0530

    fix middleware static files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,36 +1,41 @@
-import { match } from '@formatjs/intl-localematcher'
-import Negotiator from 'negotiator'
-import { NextResponse } from 'next/server'
+import { match } from "@formatjs/intl-localematcher";
+import Negotiator from "negotiator";
+import { NextResponse } from "next/server";
 
-let locales = ['en', 'fr', 'ar']
-let defaultLocale = 'en'
+let locales = ["en", "fr", "ar"];
+let defaultLocale = "en";
 
 function getLocale(request: any) {
-  let headers = { 'accept-language': request.headers.get('accept-language') }
-  let languages = new Negotiator({ headers }).languages()
-  return match(languages, locales, defaultLocale)
+  let headers = { "accept-language": request.headers.get("accept-language") };
+  let languages = new Negotiator({ headers }).languages();
+  return match(languages, locales, defaultLocale);
 }
 
 export function middleware(request: any) {
-  const { pathname } = request.nextUrl
-  
-  // Studio ignore
-  if (pathname.startsWith('/studio')) {
-    return NextResponse.next()
+  const { pathname } = request.nextUrl;
+
+  // Ignore Next.js internal paths and static files
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/studio") ||
+    pathname.includes(".")
+  ) {
+    return NextResponse.next();
   }
 
   const pathnameHasLocale = locales.some(
     (locale) => pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
-  )
+  );
 
-  if (pathnameHasLocale) return
+  if (pathnameHasLocale) return NextResponse.next();
 
-  const locale = getLocale(request)
-  request.nextUrl.pathname = `/${locale}${pathname}`
-  return NextResponse.redirect(request.nextUrl)
+  const locale = getLocale(request);
+  request.nextUrl.pathname = `/${locale}${pathname}`;
+
+  return NextResponse.redirect(request.nextUrl);
 }
 
 export const config = {
- 
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|.*\\.mp4$|.*\\.mov$).*)'],
-}
+  matcher: ["/((?!_next|api|studio).*)"],
+};
